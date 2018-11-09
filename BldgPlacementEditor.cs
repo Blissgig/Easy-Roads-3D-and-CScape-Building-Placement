464f29e882e2bf6e99bf90d02e37fcb12ee961ea
James Rose <blissgig@gmail.com>
2018-11-09 16:55:15 -0500

    The editor code
    Not much here, it calls the "BldgPlacement" class

---
+++ BldgPlacementEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(BldgPlacement))]
+public class BldgPlacementEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        BldgPlacement BldgPlace = (BldgPlacement)target;
+
+        if (GUILayout.Button("Place Buildings"))
+        {
+            BldgPlace.PlaceBuildings();
+        }
+    }
+}
